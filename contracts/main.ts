bb516e4c246deae70c0fe7608d8ccd6981bc97a3
sepezho ⠀ <sepezho@201.214.69.148.rev.vodafone.pt>
2023-02-26 14:58:50 +0000

    init

---
+++ contracts/main.ts
@@ -0,0 +1,39 @@
+import { Cell, beginCell, Address } from "ton";
+
+export function data(params: { ownerAddress: Address; number: number }): Cell {
+  return beginCell().storeAddress(params.ownerAddress).storeUint(params.number, 64).endCell();
+}
+
+function bufferToChunks(buff: Buffer, chunkSize: number) {
+  let chunks: Buffer[] = [];
+  while (buff.byteLength > 0) {
+    chunks.push(buff.slice(0, chunkSize));
+    buff = buff.slice(chunkSize);
+  }
+  return chunks;
+}
+
+export function makeSnakeCell(data: Buffer) {
+  let chunks = bufferToChunks(data, 127);
+  let rootCell = new Cell();
+  let curCell = rootCell;
+
+  for (let i = 0; i < chunks.length; i++) {
+    let chunk = chunks[i];
+
+    curCell.bits.writeBuffer(chunk);
+
+    if (chunks[i + 1]) {
+      let nextCell = new Cell();
+      curCell.refs.push(nextCell);
+      curCell = nextCell;
+    }
+  }
+
+  return rootCell;
+}
+
+export function calculate(params: { payload: string }): Cell {
+  let data = Buffer.from('8888' + params.payload);
+  return makeSnakeCell(data);
+}
---
+++ test/main.spec.ts
@@ -0,0 +1,34 @@
+import { beginCell, Cell } from "ton";
+import { SmartContract } from "ton-contract-executor";
+import { internalMessage, randomAddress } from "./helpers";
+
+import { hex } from "../build/main.compiled.json";
+
+describe("sample test", () => {
+  let contract: SmartContract;
+
+  beforeEach(async () => {
+    contract = await SmartContract.fromCell(
+      Cell.fromBoc(hex)[0] as any, // code cell from build output
+      beginCell().endCell(),
+      {
+        debug: true,
+      }
+    );
+  });
+
+  it("should not drop", async () => {
+    const tx = await contract.sendInternalMessage(
+      internalMessage({
+        from: randomAddress("notowner"),
+        body: beginCell()
+          .storeUint(69, 32) // op code
+          .storeUint(0, 32) // query
+          .endCell(),
+      }) as any
+    );
+
+    console.log(tx.debugLogs);
+    console.log(tx.gas_consumed);
+  });
+});
